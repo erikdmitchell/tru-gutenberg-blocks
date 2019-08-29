b5710692c288c4e2e84d04d9d70510c8f82bd1cb
Erik <erikdmitchell@gmail.com>
2019-08-29 10:05:18 -0400

    added stylesheets

--- blocks/race-predictions/race-predictions.php
+++ blocks/race-predictions/race-predictions.php
@@ -1,15 +1,32 @@
 <?php
 
 function tru_race_predictions_register_block() {
+    $url = TRU_GUTENBERG_BLOCKS_URL . 'blocks/race-predictions/';
     wp_register_script(
         'tru-race-predictions',
-        TRU_GUTENBERG_BLOCKS_URL . 'blocks/race-predictions/race-predictions.js',
-        array( 'wp-blocks', 'wp-element' )
+        $url . 'race-predictions.js',
+        array( 'wp-blocks', 'wp-element' ),
+        TRU_GUTENBERG_BLOCKS_VERSION
     );
  
-    register_block_type( 'tru-blocks/race-predictions', array(
-        'editor_script' => 'tru-race-predictions',
-    ) );
+    wp_register_style(
+        'tru-race-predictions-editor',
+        $url . 'editor.css',
+        array( 'wp-edit-blocks' ),
+        TRU_GUTENBERG_BLOCKS_VERSION
+    );
  
+    wp_register_style(
+        'tru-race-predictions',
+        $url . 'style.css',
+        array( ),
+        TRU_GUTENBERG_BLOCKS_VERSION
+    );
+
+    register_block_type( 'tru-blocks/race-predictions', array(
+        'style' => 'tru-race-predictions',
+        'editor_style' => 'tru-race-predictions-editor',
+        'editor_script' => 'tru-race-predictions',
+    ) ); 
 }
 add_action( 'init', 'tru_race_predictions_register_block' );
