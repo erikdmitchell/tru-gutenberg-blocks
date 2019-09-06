f819b9087d2f76f9e0d1ea95d4476174bf47cc07
Erik <erikdmitchell@gmail.com>
2019-09-06 09:01:48 -0400

    added pr2 block

---
+++ blocks/pr2/block.js
@@ -0,0 +1,39 @@
+( function( blocks, editor, element ) {
+    var el = element.createElement;
+    var registerBlockType = blocks.registerBlockType;
+    var InnerBlocks = editor.InnerBlocks;
+    
+    const blocksTemplate = [
+        [ 'core/heading', { placeholder: 'Rider Name' } ],
+        [ 'core/image', { placeholder: 'Rider Image' } ],
+        [ 'core/paragraph', { placeholder: 'Content' } ],
+    ];    
+ 
+    blocks.registerBlockType( 'tru-blocks/power-rankings', {
+        title: 'Power Rankings',
+        icon: 'list-view',
+        category: 'tru',
+        edit: ( props ) => {
+            return el( InnerBlocks, {
+                template: blocksTemplate,
+                templateLock: true
+            });
+        },
+        save: ( props ) => {
+            return el( InnerBlocks.Content, {} );
+        },   
+    } );
+}(
+    window.wp.blocks,
+    window.wp.editor,
+    window.wp.element
+) );
+
+/*
+  
+  name (string)
+  last week (int)  
+  details (editor)
+  image (image)
+    
+*/ 
