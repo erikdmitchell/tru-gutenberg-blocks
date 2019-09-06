29ff747dc780cc28cd1dd1d7d0a0b8c1250f222a
Erik <erikdmitchell@gmail.com>
2019-09-06 09:22:01 -0400

    added edit function

--- blocks/pr2/block.js
+++ blocks/pr2/block.js
@@ -79,29 +79,180 @@
         },
         edit: function (props) {
               var attributes = props.attributes
-              var alignment = props.attributes.alignment
-              var facebookURL = props.attributes.facebookURL
-              var twitterURL = props.attributes.twitterURL
-              var instagramURL = props.attributes.instagramURL
-              var linkedURL = props.attributes.linkedURL
-              var emailAddress = props.attributes.emailAddress
+              var riderName = props.attributes.riderName
         
               var onSelectImage = function (media) {
                 return props.setAttributes({
-                  mediaURL: media.url,
-                  mediaID: media.id
+                  mediaURL: media.url
                 })
               }
         
+/*
               function onChangeAlignment (newAlignment) {
                 props.setAttributes({ alignment: newAlignment })
               }
+*/
         	
               return [
         
-        	    // We build the editor interface of our block here.
-        		
+                el(BlockControls, { key: 'controls' }, // Display controls when the block is clicked on.
+                  el('div', { className: 'components-toolbar' },
+                    el(MediaUpload, {
+                      onSelect: onSelectImage,
+                      type: 'image',
+                      render: function (obj) {
+                        return el(components.Button, {
+                          className: 'components-icon-button components-toolbar__control',
+                          onClick: obj.open
+                        },
+                        // Add Dashicon for media upload button.
+                        el('svg', { className: 'dashicon dashicons-edit', width: '20', height: '20' },
+                          el('path', { d: 'M2.25 1h15.5c.69 0 1.25.56 1.25 1.25v15.5c0 .69-.56 1.25-1.25 1.25H2.25C1.56 19 1 18.44 1 17.75V2.25C1 1.56 1.56 1 2.25 1zM17 17V3H3v14h14zM10 6c0-1.1-.9-2-2-2s-2 .9-2 2 .9 2 2 2 2-.9 2-2zm3 5s0-6 3-6v10c0 .55-.45 1-1 1H5c-.55 0-1-.45-1-1V8c2 0 3 4 3 4s1-3 3-3 3 2 3 2z' })
+                        ))
+                      }
+                    })
+                  ),
+/*
+                  // Display alignment toolbar within block controls.
+                  el(AlignmentToolbar, {
+                    value: alignment,
+                    onChange: onChangeAlignment
+                  })
+*/
+                ),
+                el(InspectorControls, { key: 'inspector' }, // Display the block options in the inspector panel.
+                  el(components.PanelBody, {
+                    title: i18n.__('Social Media Links'),
+                    className: 'block-social-links',
+                    initialOpen: true
+                  },
+                  el('p', {}, i18n.__('Add links to your social media profiles.')),
+                  // Email address text field option.
+                  el(TextControl, {
+                    type: 'text',
+                    label: i18n.__('Rider Nane'),
+                    value: riderName,
+                    onChange: function (newRiderName) {
+                      props.setAttributes({ riderName: newRiderName })
+                    }
+                  }))
+                ),
+                el('div', { className: props.className },
+                  el('div', {
+                    className: attributes.mediaID ? 'organic-profile-image image-active' : 'organic-profile-image image-inactive',
+                    style: attributes.mediaID ? { backgroundImage: 'url(' + attributes.mediaURL + ')' } : {}
+                  },
+                  el(MediaUpload, {
+                    onSelect: onSelectImage,
+                    type: 'image',
+                    value: attributes.mediaID,
+                    render: function (obj) {
+                      return el(components.Button, {
+                        className: attributes.mediaID ? 'image-button' : 'button button-large',
+                        onClick: obj.open
+                      },
+                      !attributes.mediaID ? i18n.__('Upload Image') : el('img', { src: attributes.mediaURL })
+                      )
+                    }
+                  })
+                  ),
+                  el('div', { className: 'organic-profile-content', style: { textAlign: alignment } },
+                    el(RichText, {
+                      key: 'editable',
+                      tagName: 'h3',
+                      placeholder: 'Profile Name',
+                      keepPlaceholderOnFocus: true,
+                      value: attributes.title,
+                      onChange: function (newTitle) {
+                        props.setAttributes({ title: newTitle })
+                      }
+                    }),
+                    el(RichText, {
+                      key: 'editable',
+                      tagName: 'p',
+                      placeholder: i18n.__('Write a brief bio...'),
+                      keepPlaceholderOnFocus: true,
+                      value: attributes.bio,
+                      onChange: function (newBio) {
+                        props.setAttributes({ bio: newBio })
+                      }
+                    }),
+                    el('div', { className: 'organic-profile-social' },
+                      attributes.riderName && el('a', {
+                        className: 'social-link',
+                        href: 'mailto:' + attributes.riderName,
+                        target: '_blank'
+                      },
+                      el('i', { className: 'fa fa-envelope' })
+                      )
+                    )
+                  )
+                )        		
               ]
+        save: function (props) {
+            var attributes = props.attributes
+            var riderName = props.attributes.riderName
+        
+            return (
+              el('div', { className: props.className },
+                el('div', { className: 'organic-profile-image', style: { backgroundImage: 'url(' + attributes.mediaURL + ')' } },
+                  el('img', { src: attributes.mediaURL })
+                ),
+                el('div', { className: 'organic-profile-content', style: { textAlign: attributes.alignment } },
+                  el(RichText.Content, {
+                    tagName: 'h3',
+                    value: attributes.title
+                  }),
+                  el(RichText.Content, {
+                    tagName: 'h5',
+                    value: attributes.subtitle
+                  }),
+                  el(RichText.Content, {
+                    tagName: 'p',
+                    value: attributes.bio
+                  }),
+                  el('div', { className: 'organic-profile-social' },
+                    attributes.facebookURL && el('a', {
+                      className: 'social-link',
+                      href: attributes.facebookURL,
+                      target: '_blank'
+                    },
+                    el('i', { className: 'fa fa-facebook' })
+                    ),
+                    attributes.twitterURL && el('a', {
+                      className: 'social-link',
+                      href: attributes.twitterURL,
+                      target: '_blank'
+                    },
+                    el('i', { className: 'fa fa-twitter' })
+                    ),
+                    attributes.instagramURL && el('a', {
+                      className: 'social-link',
+                      href: attributes.instagramURL,
+                      target: '_blank'
+                    },
+                    el('i', { className: 'fa fa-instagram' })
+                    ),
+                    attributes.linkedURL && el('a', {
+                      className: 'social-link',
+                      href: attributes.linkedURL,
+                      target: '_blank'
+                    },
+                    el('i', { className: 'fa fa-linkedin' })
+                    ),
+                    attributes.emailAddress && el('a', {
+                      className: 'social-link',
+                      href: 'mailto:' + attributes.emailAddress,
+                      target: '_blank'
+                    },
+                    el('i', { className: 'fa fa-envelope' })
+                    )
+                  )
+                )
+              )
+            )
+          }
+        })
         },    
     } );  	
 
