a168722caa581a325f0049d5af771dff72108f80
Erik <erikdmitchell@gmail.com>
2019-09-06 09:14:17 -0400

    setup block registration

--- blocks/pr2/block.js
+++ blocks/pr2/block.js
@@ -1,3 +1,4 @@
+/*
 ( function( blocks, editor, element ) {
     var el = element.createElement;
     var registerBlockType = blocks.registerBlockType;
@@ -28,6 +29,7 @@
     window.wp.editor,
     window.wp.element
 ) );
+*/
 
 /*
   
@@ -37,3 +39,76 @@
   image (image)
     
 */ 
+
+
+(function (blocks, editor, components, i18n, element) {
+	var el = wp.element.createElement;
+  	var registerBlockType = wp.blocks.registerBlockType;
+  	var RichText = wp.editor.RichText;
+  	var BlockControls = wp.editor.BlockControls;
+  	var AlignmentToolbar = wp.editor.AlignmentToolbar;
+  	var MediaUpload = wp.editor.MediaUpload;
+  	var InspectorControls = wp.editor.InspectorControls;
+  	var TextControl = components.TextControl;
+  	
+    registerBlockType('tru-blocks/pr2', { 
+        title: i18n.__('Power Ranking Rider'),
+        description: i18n.__('A custom block for displaying a rider in a power ranking.'),
+        icon: 'list-view',
+        category: 'tru',
+        attributes: { 
+          title: {
+            type: 'array',
+            source: 'children',
+            selector: 'h3'
+          },
+          bio: {
+            type: 'array',
+            source: 'children',
+            selector: 'p'
+          },
+          mediaURL: {
+            type: 'string',
+            source: 'attribute',
+            selector: 'img',
+            attribute: 'src'
+          },
+          riderName: {
+            type: 'text'
+          }
+        },
+        edit: function (props) {
+              var attributes = props.attributes
+              var alignment = props.attributes.alignment
+              var facebookURL = props.attributes.facebookURL
+              var twitterURL = props.attributes.twitterURL
+              var instagramURL = props.attributes.instagramURL
+              var linkedURL = props.attributes.linkedURL
+              var emailAddress = props.attributes.emailAddress
+        
+              var onSelectImage = function (media) {
+                return props.setAttributes({
+                  mediaURL: media.url,
+                  mediaID: media.id
+                })
+              }
+        
+              function onChangeAlignment (newAlignment) {
+                props.setAttributes({ alignment: newAlignment })
+              }
+        	
+              return [
+        
+        	    // We build the editor interface of our block here.
+        		
+              ]
+        },    
+    } );  	
+
+})(
+  window.wp.blocks,
+  window.wp.editor,
+  window.wp.components,
+  window.wp.i18n,
+  window.wp.element
+)
