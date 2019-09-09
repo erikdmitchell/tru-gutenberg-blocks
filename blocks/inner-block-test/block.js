4cab4b474414ccca53898e9f1213572f48208425
Erik <erikdmitchell@gmail.com>
2019-09-09 09:42:18 -0400

    added side panel controls

--- blocks/inner-block-test/block.js
+++ blocks/inner-block-test/block.js
@@ -1,11 +1,98 @@
 (function (blocks, editor, components, i18n, element) {
-	var el = wp.element.createElement;
-  	var registerBlockType = wp.blocks.registerBlockType;
-  	var RichText = wp.editor.RichText;
-  	var MediaUpload = wp.editor.MediaUpload;
-  	var TextControl = components.TextControl;
-  	var IconButton = components.IconButton;
-  	
+    const el = wp.element.createElement;
+    const { registerBlockType } = wp.blocks;
+    const { InnerBlocks } = wp.editor;
+	const { RichText, InspectorControls } = editor;
+	const { Fragment } = element;
+    const {
+    	TextControl,
+    	CheckboxControl,
+    	RadioControl,
+    	SelectControl,
+    	TextareaControl,
+    	ToggleControl,
+    	RangeControl,
+    	Panel,
+    	PanelBody,
+    	PanelRow
+    } = components;
+     
+    const BLOCKS_TEMPLATE = [
+        [ 'core/image', {} ],
+        [ 'core/paragraph', { placeholder: 'Image Details' } ],
+    ];
+
+    registerBlockType( 'tru-blocks/template', {
+        title: 'My Template Block',
+        category: 'tru',
+        attributes: {
+        	list_id: {
+        		type: 'string',
+        		// default: '12345' // you can set a default value
+        	},
+        	doubleoptin: {
+        		type: 'boolean' // for ToggleControl we need boolean type
+        	}
+        },       
+        edit: function( props ) {
+         
+        	return (
+        		el( Fragment, {},
+        			el( InspectorControls, {},
+        				el( PanelBody, { title: 'Form Settings', initialOpen: true },
+         
+        					/* Text Field */
+        					el( PanelRow, {},
+        						el( TextControl,
+        							{
+        								label: 'List ID',
+        								onChange: ( value ) => {
+        									props.setAttributes( { list_id: value } );
+        								},
+        								value: props.attributes.list_id
+        							}
+        						)
+        					),
+         
+        					/* Toggle Field */
+        					el( PanelRow, {},
+        						el( ToggleControl,
+        							{
+        								label: 'Double Opt In',
+        								onChange: ( value ) => {
+        									props.setAttributes( { doubleoptin: value } );
+        								},
+        								checked: props.attributes.doubleoptin,
+        							}
+        						)
+        					)
+         
+        				),
+         
+        			),
+         
+        			/*  
+        			 * Here will be your block markup 
+        			 */
+                     el( InnerBlocks, {
+                        template: BLOCKS_TEMPLATE,
+                        templateLock: false
+                     })
+        		)
+            )
+        },
+        save: ( props ) => {
+            return el( InnerBlocks.Content, {} );
+        },
+    }); 
+    
+/*
+                     el( InnerBlocks, {
+                        template: BLOCKS_TEMPLATE,
+                        templateLock: false
+                     })
+*/     	
+/*
     registerBlockType('tru-blocks/power-ranking-rider', { 
         title: i18n.__('Power Ranking Rider'),
         description: i18n.__('A custom block for displaying a rider in a power ranking.'),
@@ -20,9 +107,10 @@
         save: function (props) {
             var attributes = props.attributes;
 
-        return ();
-    }
-  })	
+            return ();
+        }
+    })	
+*/
 
 })(
   window.wp.blocks,
@@ -31,3 +119,4 @@
   window.wp.i18n,
   window.wp.element
 )
+
