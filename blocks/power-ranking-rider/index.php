9d5780946f8926b66d15cd20ea6994039a4132d0
Erik <erikdmitchell@gmail.com>
2019-09-10 09:30:56 -0400

    split styles to load on frontend and backend only

--- blocks/power-ranking-rider/index.php
+++ blocks/power-ranking-rider/index.php
@@ -10,20 +10,6 @@ function tru_power_ranking_rider_register_block() {
         TRU_GUTENBERG_BLOCKS_VERSION
     );
  
-    wp_register_style(
-        'tru-power-ranking-rider-editor',
-        $url . 'editor.css',
-        array( 'wp-edit-blocks' ),
-        TRU_GUTENBERG_BLOCKS_VERSION
-    );
- 
-    wp_register_style(
-        'tru-power-ranking-rider-style',
-        $url . 'style.css',
-        array( ),
-        TRU_GUTENBERG_BLOCKS_VERSION
-    );
-
     register_block_type( 'tru-blocks/power-ranking-rider', array(
         'style' => 'tru-power-ranking-rider-style',
         'editor_style' => 'tru-power-ranking-rider-editor',
@@ -31,3 +17,27 @@ function tru_power_ranking_rider_register_block() {
     ) ); 
 }
 add_action( 'init', 'tru_power_ranking_rider_register_block' );
+
+function tru_power_ranking_rider_block_editor_styles() {
+    $url = TRU_GUTENBERG_BLOCKS_URL . 'blocks/power-ranking-rider/';
+    
+    wp_register_style(
+        'tru-power-ranking-rider-editor',
+        $url . 'editor.css',
+        array( 'wp-edit-blocks' ),
+        TRU_GUTENBERG_BLOCKS_VERSION
+    );    
+}
+add_action( 'enqueue_block_editor_assets', 'tru_power_ranking_rider_block_editor_styles' );
+
+function tru_power_ranking_rider_block_frontend_styles() {
+    $url = TRU_GUTENBERG_BLOCKS_URL . 'blocks/power-ranking-rider/';
+    
+    wp_register_style(
+        'tru-power-ranking-rider-style',
+        $url . 'style.css',
+        array( ),
+        TRU_GUTENBERG_BLOCKS_VERSION
+    );    
+}
+add_action( 'enqueue_block_assets', 'tru_power_ranking_rider_block_frontend_styles' );
